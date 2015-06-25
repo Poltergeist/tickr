ab4fb28ceee4253ac1b0a22d20423ef5ab5de1fb
Sp3c1 <spectator.networx@gmail.com>
2015-06-25 15:40:30 +0200

    feat(sinon): add sinon for mocks.

--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -1,7 +1,15 @@
-/* global describe, it, expect */
-var Ticker = require('../index.js');
+/* global describe, it, expect, afterEach, beforeEach */
+var Ticker = require('../index.js'),
+  sinon = require('sinon');
 
 describe('Time', function() {
+  var clock;
+  beforeEach(function() {
+    clock = sinon.useFakeTimers(new Date() * 1);
+  });
+  afterEach(function() {
+    clock.restore();
+  });
   it('should return the right time', function() {
     var tickr = new Ticker();
     expect((tickr.getCurrentTime() % 1000)).toBe(new Date() % 1000);
@@ -26,6 +34,18 @@ describe('Time', function() {
       expect(tickr.getCurrentTick()).toBe(noOfTicks);
       done();
     }, tick * noOfTicks);
+    clock.tick(tick * noOfTicks);
+  });
+  it('should have the right tick after a lot of time', function(done) {
+    var now = new Date(),
+      tick = 1000 * 60 * 5,
+      noOfTicks = 400,
+      tickr = new Ticker(now, {tick: tick});
+    setTimeout(function() {
+      expect(tickr.getCurrentTick()).toBe(noOfTicks);
+      done();
+    }, tick * noOfTicks);
+    clock.tick(tick * noOfTicks);
   });
   it('should have the right tick after specific time', function() {
     var now = new Date(),
